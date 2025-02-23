14d83e93da0e7939bad1d6aad03183b31e0cb63d
seyoo630 <165480675+seyoo630@users.noreply.github.com>
2025-02-23 13:30:41 +0900

    s2_23_1

--- Assets/02.Scripts/DinoController.cs
+++ Assets/02.Scripts/DinoController.cs
@@ -1,36 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DinoController : MonoBehaviour
 {
+    [Header("Settings")]
     public float jumpPower = 2f;
     public bool isDown = false;
     public bool isGround = true;
-    // Start is called before the first frame update
+    public bool isShootable = true;
+
+    [Header("References")]
+    public GameObject[] GunPrefabs;
+    public Button SpawnButton;
+    public Transform ArrowRangePoint;
+    public Transform groundCheckPoint;
+    public Transform SpawnCheckPoint;
+    public LayerMask whatIsGround; //ground인지 비교할 Mask
+    public LayerMask whatIsEnemy;
+
+    [SerializeField]
+    [Tooltip("boxCollider offset과 size를 변경")]
+    private Vector2 saveOffset;// 서있을때 offset
+    private Vector2 saveSize;// 서있을때 BoxCollider의 Size
+    
     private Animator Anim;
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private List<Vector2> spawnPositions;
 
 
-    public Transform groundCheckPoint;
-    public LayerMask whatIsGround; //ground인지 비교할 Mask
+
+
+
 
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
         Anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
 
+        SaveColliderSettings();
+        InitializeSpawnPositions();
+
+
         Anim.SetBool("isGround", true);
+
+        SpawnButton.onClick.AddListener(SpawnGunPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
         isGround = Physics2D.OverlapCircle(groundCheckPoint.position, 0.2f, whatIsGround);
+        isShootable = Physics2D.OverlapCircle(ArrowRangePoint.position, 5f, whatIsEnemy);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGround.Equals(true))
         {
-            
+            if(!isDown)
             //rb.AddForce(Vector2.up * jumpPower , ForceMode2D.Impulse);
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
             //Anim.SetBool("isGround", false);
@@ -38,18 +66,46 @@ public class DinoController : MonoBehaviour
 
         Anim.SetBool("isGround", isGround);
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && isGround.Equals(true))
         {
-            isDown = !isDown;
-            Anim.SetBool("isDown", true);
+            SetDownArrowDown();
+            
         }
 
-        if (Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKeyUp(KeyCode.DownArrow) && isGround.Equals(true))
         {
-            isDown = !isDown;
-            Anim.SetBool("isDown", false);
+            SetDownArrowUp();
+            
         }
 
+    }
+
+    void SaveColliderSettings()
+    {
+        saveOffset = boxCollider.offset;
+        saveSize = boxCollider.size;
+    }
+
+    void LoadColliderSettings()
+    {
+        boxCollider.offset = saveOffset;
+        boxCollider.size = saveSize;
+    }
+
+    void SetDownArrowDown()
+    {
+        Anim.SetBool("isDown", true);
+
+        boxCollider.offset = new Vector2(0f, -0.2f);
+        boxCollider.size = new Vector2(1.76f, 0.86f);
+        isDown = true;
+    }
+
+    void SetDownArrowUp()
+    {
+        Anim.SetBool("isDown", false);
+        LoadColliderSettings();
+        isDown = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -58,5 +114,56 @@ public class DinoController : MonoBehaviour
         {
             Anim.SetBool("isGround", true);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheckPoint.position, 0.2f);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(ArrowRangePoint.position, 7f);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(SpawnCheckPoint.position, 2f);
+    }
+
+    void InitializeSpawnPositions()
+    {
+        spawnPositions = new List<Vector2>();
+        int pointCount = 8;
+        float radius = 2f;
+        Vector2 center = SpawnCheckPoint.position;
+
+        for (int i = 0; i < pointCount; i++)
+        {
+            float angle = i * (360f / pointCount) * Mathf.Deg2Rad;
+            float x = center.x + radius * Mathf.Cos(angle);
+            float y = center.y + radius * Mathf.Sin(angle);
+            spawnPositions.Add(new Vector2(x, y));
+        }
+    }
+
+    
+    void SpawnGunPrefab()
+    {
+        if (spawnPositions.Count == 0)
+        {
+            Debug.Log("더 이상 사용할 스폰 좌표가 없습니다.");
+            return;
+        }
+
+        
+        Vector2 spawnPosition = spawnPositions[0];
+        spawnPositions.RemoveAt(0);
+
+        int randomIndex = Random.Range(0, GunPrefabs.Length);
+        Instantiate(GunPrefabs[randomIndex], spawnPosition, Quaternion.identity);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("뭔가 충돌");
     }
 }
---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("References")]
+    public Transform[] spawnPoints;
+    public GameObject[] obstacles;
+
+    public float spawnInterval = 2f;
+    public float spawnDefault = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnDefault += Time.deltaTime;
+
+        if(spawnDefault >= spawnInterval)
+        {
+            SpawnObstacle();
+            spawnDefault = 0;
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        int RandomIndex = Random.Range(0, obstacles.Length);
+
+        if(RandomIndex == 0)
+        {
+            Instantiate(obstacles[RandomIndex], spawnPoints[0].position, Quaternion.identity);
+        }
+        else if(1 <= RandomIndex && RandomIndex <=3)
+        {
+            Instantiate(obstacles[RandomIndex], spawnPoints[1].position, Quaternion.identity);
+        }
+        else
+        {
+            int randomSpawnIndex = Random.Range(2, spawnPoints.Length);
+            Instantiate(obstacles[RandomIndex], spawnPoints[randomSpawnIndex].position, Quaternion.identity);
+        }
+    }
+}
